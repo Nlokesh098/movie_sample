319ae3cc93fa0314cc522e3b9d09778ed83bd882
Nlokesh Nelluri <lokeshnelluri9@gmail.com>
2026-01-29 11:32:09 -0600

    log function

--- src/main/java/org/example/functions/Addimpl.java
+++ src/main/java/org/example/functions/Addimpl.java
@@ -1,10 +1,13 @@
 package org.example.functions;
 
+import jdk.internal.org.jline.terminal.TerminalBuilder;
+
 public class Addimpl implements addservice {
 
 
     @Override
     public int add(int a, int b) {
+        System.out.println("addition is being performed");
         return a + b;
     }
 }
